5940c6cd3287ea4886878ea85311c450257b6146
Andrew-Zakharov <zakharov-1997@yandex.by>
2017-04-12 11:34:56 +0300

    Added thinning by frequency

--- Fourier/FastFourierTransform.cpp
+++ Fourier/FastFourierTransform.cpp
@@ -8,7 +8,7 @@ FastFourierTransform::FastFourierTransform()
 
 QVector<complex<double>> *FastFourierTransform::transform(const QVector<std::complex<double>> &data)
 {
-    if (data.size() == 1)
+    /*if (data.size() == 1)
     {
         QVector<complex<double>> *vector = new QVector<complex<double>>(1);
         (*vector)[0] = data[0];
@@ -51,7 +51,42 @@ QVector<complex<double>> *FastFourierTransform::transform(const QVector<std::com
         additionCounter += 2;
     }
 
-    return y;
+    return y;*/
+    if (data.size() == 1)
+        {
+            QVector<std::complex<double>> *vector = new QVector<std::complex<double>>(1);
+            (*vector)[0] = data[0];
+            return vector;
+        }
+        QVector<std::complex<double>> b(data.size()/2);
+        QVector<std::complex<double>> c(data.size()/2);
+
+        std::complex<double> e(exp(1.0), 0);
+        complex<double> wn = pow(e, -2. * complex<double>(0,1) * M_PI / static_cast<double>(data.size()));
+        std::complex<double> w(1, 0);
+
+         multiplyCounter += 4;
+
+        for (int i = 0;i<data.size()/2;i++)
+        {
+            b[i] = data[i] + data[i + data.size()/2];
+            c[i] = w * (data[i] - data[i + data.size()/2]);
+            w = w * wn;
+            multiplyCounter += 3;
+            additionCounter += 2;
+        }
+
+        QVector<std::complex<double>> *nb = transform(b);
+        QVector<std::complex<double>> *nc = transform(c);
+        QVector<std::complex<double>> *y = new QVector<std::complex<double>>(data.size());
+        for (int i = 0;i<data.size()/2;i++)
+        {
+            (*y)[2*i] = (*nb)[i];
+            (*y)[2*i+1] = (*nc)[i];
+        }
+        delete nb;
+        delete nc;
+        return y;
 }
 
 QVector<std::complex<double> > *FastFourierTransform::directTransform(const QVector<complex<double> > &data)
